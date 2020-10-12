11669e1efcc899e806199d7c82c83ce554f4f5d7
Thai Tan Loi <thaitanloi365@gmail.com>
2020-10-12 13:59:11 +0700

    edit test

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,9 +5,9 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example.org www.example.org)
+domains=(thaitanloi.me www.thaitanloi.me)
 rsa_key_size=4096
-data_path="./data/certbot"
+data_path="./certbot"
 email="" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 

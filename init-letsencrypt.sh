b200500f7cb6727083d22b123940d4c1c80d29cf
Thái Tấn Lợi <thaitanloi365@gmail.com>
2020-10-12 23:52:56 +0700

    change domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(thaitanloi.me www.thaitanloi.me)
+domains=(test.northgate.app www.test.northgate.app)
 rsa_key_size=4096
 data_path="./certbot"
 email="" # Adding a valid address is strongly recommended

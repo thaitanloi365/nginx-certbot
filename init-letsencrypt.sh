6254441d3e5b286c0b03448d8843aa0adf58fe82
Thái Tấn Lợi <thaitanloi365@gmail.com>
2020-10-12 23:49:51 +0700

    edit scripts

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,11 +5,11 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=${1}
+domains=(thaitanloi.me www.thaitanloi.me)
 rsa_key_size=4096
-data_path=${2}
+data_path="./certbot"
 email="" # Adding a valid address is strongly recommended
-staging=${3:-1} # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
